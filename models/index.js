a83cc26cfc20eedadb308579793a7fdca5350d62
finntendoverse <finnlambouris616@gmail.com>
2024-05-01 21:43:35 -0400

    added table joins in models index file

--- models/index.js
+++ models/index.js
@@ -4,13 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'categoryId',
+  onDelete: "CASCADE",
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "categoryId"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  as: "productTags"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  as: "tagProducts"
+});
 
 module.exports = {
   Product,
