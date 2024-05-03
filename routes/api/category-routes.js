1b593e03bc8a2ee27b3e87bec380b49dc29e56ff
finntendoverse <finnlambouris616@gmail.com>
2024-05-02 22:16:49 -0400

    completed all Category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,21 +16,62 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // find one category by its `id` value
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+    return res.status(200).json(categoryData);
+  } catch {
+    return res.status(404).send("There is an error with the /api/categories/:id GET route.");
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    // create a new category
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    return res.status(200).json(categoryData);
+  } catch {
+    return res.status(404).send("There is an error with the /api/categories/ POST route.");
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    // update a category by its `id` value
+    let categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+    return res.status(200).json(categoryData);
+  } catch {
+    return res.status(404).send("There is an error with the /api/categories/:id PUT route.");
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    // delete a category by its `id` value
+    let categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    return res.status(200).json(categoryData);
+  } catch {
+    return res.status(404).send("There is an error with the /api/categories/:id PUT route.");
+  }
 });
 
 module.exports = router;
